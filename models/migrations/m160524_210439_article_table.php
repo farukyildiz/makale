c218eaaf448785f9f96e7b1463e163530760bb7c
Faruk Yıldız <farukyildiz.079@gmail.com>
2016-05-25 11:12:32 +0300

    Update m160524_210439_article_table.php

--- models/migrations/m160524_210439_article_table.php
+++ models/migrations/m160524_210439_article_table.php
@@ -5,15 +5,15 @@ use yii\db\Migration;
 class m160524_210439_article_table extends Migration
 {
     public function up()
-    {
-		$this->execute('CREATE TABLE IF NOT EXISTS `article` (
+    {		$ss = 'CREATE TABLE IF NOT EXISTS `article` (
 			`id` int(11) NOT NULL AUTO_INCREMENT,
 			`author_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
 			`article_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
 			`category` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
 			`content` varchar(10000) COLLATE utf8_unicode_ci NOT NULL,
 			PRIMARY KEY (`id`)
-			) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;');
+			) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;';
+			$this->execute($ss);
     }
 
     public function down()
